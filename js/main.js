dcc8b6a30af0e22f98f6764a7aef12d6ed715d95
Noushed Ahmed Jholok <80415299+Enexcode@users.noreply.github.com>
2025-01-05 23:56:45 +0600

    history_page

--- js/main.js
+++ js/main.js
@@ -51,3 +51,13 @@ $('.banner_main_slider').slick({
     dots: true,
     // fade: true,
   });
+
+
+  // Bottom Menu Active Js 
+  const list = document.querySelectorAll(".nav__item");
+list.forEach((item) => {
+  item.addEventListener("click", () => {
+    list.forEach((item) => item.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
